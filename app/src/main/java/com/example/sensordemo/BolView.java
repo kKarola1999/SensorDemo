d62e5978ce3d2b6f61995483c6154b2cf1a7dd04
Karolina <250013@student.pwr.edu.pl>
2021-01-28 15:26:11 +0100

    commit

---
+++ app/src/main/java/com/example/sensordemo/BolView.java
@@ -0,0 +1,66 @@
+package com.example.sensordemo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.SensorEvent;
+import android.view.View;
+
+public class BolView extends View {
+
+    private Paint ballPaint;
+    public float x;
+    public float y;
+    private int viewWidth;
+    private int viewHight;
+
+
+    private static final int CIRCLE_RADIUS=50;
+
+
+    public BolView(Context context) {
+        super(context);
+        ballPaint=new Paint();
+        ballPaint.setColor(Color.BLACK);
+
+
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewWidth=w;
+        viewHight=h;
+    }
+
+    public void onSensorEvent(SensorEvent event){
+
+
+        if(x <= CIRCLE_RADIUS){
+            x = CIRCLE_RADIUS;
+        }
+        if(x >= viewWidth - CIRCLE_RADIUS){
+            x = viewWidth - CIRCLE_RADIUS;
+        }
+
+        if(y <= CIRCLE_RADIUS){
+            y = CIRCLE_RADIUS;
+        }
+        if(y >= viewHight - CIRCLE_RADIUS){
+            y = viewHight - CIRCLE_RADIUS;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+
+
+        canvas.drawCircle(x,y,CIRCLE_RADIUS,ballPaint);
+        ballPaint.setTextSize(32);
+        invalidate();
+    }
+
+}
---
+++ app/src/main/java/com/example/sensordemo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.sensordemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private static final String TAG="EDUIB";
+    private long startTime;
+    private BolView ballView;
+    private int frameTime=5;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        ballView=new BolView(this);
+        setContentView(ballView);
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+
+
+            float [] values=event.values;
+
+            ballView.x= ballView.x-(int)values[0]*frameTime;
+            ballView.y=ballView.y +(int)values[1]*frameTime;
+            ballView.onSensorEvent(event);
+        }
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_GAME);
+        startTime=System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
